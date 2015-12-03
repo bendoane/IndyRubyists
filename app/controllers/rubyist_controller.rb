4c9816407a0f0a554b5ed3c086540f7c6e901b44
bendoane <ben.doane@gmail.com>
2015-12-03 09:25:47 -0500

    still working on getting the info displayed from the API

--- app/controllers/rubyist_controller.rb
+++ app/controllers/rubyist_controller.rb
@@ -1,7 +1,8 @@
 class RubyistController < ApplicationController
 
   def index
-    @rubyists = Octokit.search_users("location:Indianapolis language:Ruby", :sort => "all_users")[:items]
+    rubyists = Octokit.search_users("location:Indianapolis language:Ruby", :sort => "all_users")[:items]
+    @user_info = rubyists.collect{|r| Octokit.user(r[:id])}
   end
 
 end
